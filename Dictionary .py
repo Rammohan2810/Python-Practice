b022a78cf66505964f5f16083021e5095a7f9434
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-04-29 16:28:39 +0530

    Dictionary

---
+++ Dictionary .py
@@ -0,0 +1,11 @@
+# Dictionaries are used to store data values in key:value pairs
+
+# Changeable and do not allow duplicates
+
+dict_1 = {"brand": "Ford","model": "Mustang","year": 1964}
+
+
+dict_2 = {"brand": "Ford","model": "Mustang","year": 1964}
+
+print(dict_1)
+print("Brand name is",dict_2["brand"])
