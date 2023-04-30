4022cd9d54fb28db38a071c7589c29a459a3f888
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-04-30 20:24:27 +0530

    Compare The Lists

---
+++ list_compare.py
@@ -0,0 +1,15 @@
+#Compare to two lists
+
+list_1 = [20, 40, 60, 80]
+list_2 = [20, 40, 60, 80]
+
+print(list_1 == list_2)
+print(list_1 is list_2)
+
+#Creating a Multi-Dimensional List
+
+List =[ ['Apple', 'Mango'], ['Banana']] 
+ 
+print("Accessing a element from a Multi-Dimensional list")
+print(List[0][1])
+print(List[1][0])
