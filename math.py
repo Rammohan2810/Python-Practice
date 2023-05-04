132e7af730c1f4780d191c75e51a021ec527f7a0
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-04 15:05:45 +0530

    math module

---
+++ math.py
@@ -0,0 +1,15 @@
+import math  
+
+a=math.sqrt(36)
+b=math.log(10)
+#Round a number upward to its nearest integer
+c=math.ceil(4.6)
+#Round a number downward to its nearest integer
+d=math.floor(6.8)
+e=math.pow(2,4)
+ 
+print("Sqrt value is-",a)
+print("log value is-",b)
+print("Ceil value value is-",c)
+print("floor value is-",d)
+print("pow value is-",e)
