d8baaf9f76489b9f6c550b61cffe46063f9a4a4a
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-07 19:34:01 +0530

    Lambda Function

---
+++ lambda.py
@@ -0,0 +1,9 @@
+'''A lambda function is a small anonymous function.
+A lambda function can take any number of arguments,
+but can only have one expression.'''
+
+x = lambda a, b : a * b
+print(x(5, 6))
+
+y = lambda a, b, c : a + b + c
+print(y(5, 6, 2))
