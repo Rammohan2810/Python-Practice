b346272f88a7085fad9a1427b8760ece9a473a33
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-11 21:32:26 +0530

    Right shift and Left shift

---
+++ shift.py
@@ -0,0 +1,10 @@
+a = 4
+b = 11
+print(a | b)
+'''Bitwise right shift operator(>>)
+The a’s value is moved right by the 2 bits.'''
+print(a >> 2)
+'''Bitwise left shift operator(>>)
+The a’s value is moved left by the 2 bits.'''
+print(a << 2)
+
