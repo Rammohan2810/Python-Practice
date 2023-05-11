016ebbc883471ca7701336fbfb1a219eff50a0d3
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-05-11 21:42:34 +0530

    String Operator

---
+++ String.py
@@ -0,0 +1,11 @@
+str1 = "PythonDeveloper"
+print(str1[1:4], str1[:5], str1[4:], str1[0:-1], str1[:-1])
+
+myString = "pynative"
+stringList = ["abc", "pynative", "xyz"]
+
+print(stringList[1] == myString)
+print(stringList[1] is myString)
+
+str = "my name is James bond";
+print (str.capitalize())
