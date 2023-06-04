c32df06d228bb364f32c1c9a2bf9319fa1a73b0a
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-04 21:01:52 +0530

    factorial concept

--- factorial.py
+++ factorial.py
@@ -8,8 +8,7 @@ def factorial(n):
     if (n==1 or n==0):
           return 1
     else:
-         return (n * factorial(n - 1))
- 
+         return (n * factorial(n-1))
 num = 6
 print("number : ",num)
 print("Factorial : ",factorial(num))
