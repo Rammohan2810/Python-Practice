bed1c175b2021096de53e2c9d69f69b42aefa472
RAMMOHAN R <130207215+Rammohan2810@users.noreply.github.com>
2023-06-05 20:49:27 +0530

    Set

--- set.py
+++ set.py
@@ -1,13 +1,13 @@
           # Sets
-#Sets cannot have two items with the same value
+# Sets cannot have two items with the same value
 
 set_1 = {"apple", "banana", "cherry", "apple"} 
 
-#True and 1 is considered the same value
+# True and 1 is considered the same value
 
 set_2 = {"apple", "banana", "cherry", True, 1, 2}
  
-#To determine how many items a set has, use the len() function.
+# To determine how many items a set has, use the len() function.
 
 set_3 = {"apple", "banana", "cherry"}
 
